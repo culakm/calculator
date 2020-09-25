c084d3c0a6a1c00b2b51937fa3c4b3f09dbf87ea
Martin Culak <culak.martin@gmail.com>
2020-09-25 13:58:31 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+/**
+ * Hlavní komponenta celé aplikace.
+ * @export
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'Kalkulačka v Angular frameworku';
 }
